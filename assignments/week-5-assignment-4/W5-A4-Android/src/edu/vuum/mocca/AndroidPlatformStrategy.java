56507cbc400fab21ca838f3cdde7e1418103c352
ranmeus <hktk@gmx.de>
2014-07-09 00:29:56 +0200

    my solution for the AndroidPlatformStrategy

--- assignments/week-5-assignment-4/W5-A4-Android/src/edu/vuum/mocca/AndroidPlatformStrategy.java
+++ assignments/week-5-assignment-4/W5-A4-Android/src/edu/vuum/mocca/AndroidPlatformStrategy.java
@@ -5,6 +5,8 @@ import java.util.concurrent.CountDownLatch;
 
 import android.app.Activity;
 import android.widget.TextView;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 /**
@@ -47,6 +49,7 @@ public class AndroidPlatformStrategy extends PlatformStrategy
     {
         /** (Re)initialize the CountDownLatch. */
         // TODO - You fill in here.
+    	mLatch = new CountDownLatch(NUMBER_OF_THREADS);
     }
 
     /** Print the outputString to the display. */
@@ -57,18 +60,30 @@ public class AndroidPlatformStrategy extends PlatformStrategy
          * and appends the outputString to a TextView. 
          */
         // TODO - You fill in here.
+    	(new Handler(Looper.getMainLooper())).post(new Runnable() {
+			
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				mTextViewOutput.append(outputString + "\n");
+			}
+		});
     }
 
     /** Indicate that a game thread has finished running. */
     public void done()
     {	
         // TODO - You fill in here.
+    	mLatch.countDown();
     }
 
     /** Barrier that waits for all the game threads to finish. */
     public void awaitDone()
     {
         // TODO - You fill in here.
+    	try{
+    		mLatch.await();
+    	}catch(java.lang.InterruptedException e){}
     }
 
     /** 
